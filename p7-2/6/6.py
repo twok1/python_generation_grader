5d056f4b7118d7149ed6e2d4285c37b79a9ad821
twok1 <twokey25@gmail.com>
2025-01-31 15:36:43 +0300

    3101

--- p7-2/6/6.py
+++ p7-2/6/6.py
@@ -1,16 +1,28 @@
 class FieldTracker:
-    def __init__(self, *args):
-        self._default_attrs = dict()
-        self._changed_attrs = dict()
+    _default_attrs = {}
+    _changed_attrs = {}
 
-    def base(self):
-        pass
+    def base(self, attr):
+        return self._default_attrs[attr]
 
-    def has_changed(self):
-        pass
+    def has_changed(self, attr):
+        return attr in self._changed_attrs
 
     def changed(self):
-        pass
+        return {i: self._default_attrs[i] for i in self._changed_attrs}
 
     def save(self):
-        pass
+        self._default_attrs.update(self._changed_attrs)
+        self._changed_attrs = dict()
+
+    def __setattr__(self, name, value):
+        if name in self.fields and name not in self._default_attrs:
+            self._default_attrs[name] = value
+            self.__dict__[name] = value
+        elif name in self._default_attrs and self._default_attrs[name] != value:
+            self.__dict__[name] = value
+            self._changed_attrs[name] = value
+        elif name not in self._default_attrs:
+            self.__dict__[name] = value
+        elif name in self._default_attrs and self._default_attrs[name] == value:
+            self._changed_attrs.pop(name)
