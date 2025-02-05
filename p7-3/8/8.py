5e74289ec7a5a487e9ff6a2216e051baa2c5b574
twok1 <twokey25@gmail.com>
2025-02-05 14:09:04 +0300

    52

--- p7-3/8/8.py
+++ p7-3/8/8.py
@@ -1,5 +1,4 @@
 class ModularTuple(tuple):
-    def __new__(cls, iterable, size=100):
-        iterable = tuple(map(lambda x: x / size, iterable))
-        super().__new__(cls, iterable)
-
+    def __new__(cls, iterable=(), size=100):
+        iterable = tuple(map(lambda x: x % size, iterable))
+        return super().__new__(cls, iterable)
