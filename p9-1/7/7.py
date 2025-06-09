5fc8634e2131fad7de73b261dc37bd64dac96c3c
Sega <neroznikovsergei@yandex.ru>
2025-06-09 06:37:07 +0300

    0906

---
+++ p9-1/7/7.py
@@ -0,0 +1,9 @@
+class Pagination:
+    def __init__(self, pages, visible_pages):
+        self.pages = pages
+        self.visible_pages = visible_pages
+        self._now_page = pages[0]
+
+    def get_visible_items(self):
+        return self.
+    
